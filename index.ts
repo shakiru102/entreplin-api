e7bef9f83961287db61ffbb85ed290d3b2400719
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-06-02 14:19:27 -0700

    server initiation

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { CorsOptions } from "cors"
+import express, { Request, Response } from  'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import env  from 'dotenv'
+
+env.config()
+
+const params: CorsOptions = {
+   credentials: true,
+   origin: '*',
+}
+// @ts-ignore
+mongoose.connect(process.env.MONGODB_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+.then(() => console.log('database  is connected'))
+.catch(err => console.log(err))
+
+const app = express()
+const Port = 3000 || process.env.PORT
+app.listen(Port, () => console.log('listening on port ' + Port))
+app.use(cors(params))
+
+
+app.get('/', (req: Request, res: Response) => res.send('server is running.'))

e6a8476b544b1be78d82d8945970e59503d103a9
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-09-29 17:11:02 +0100

    bug-fix

--- utils/fileStorage.ts
+++ utils/fileStorage.ts
@@ -3,14 +3,14 @@ import multer from "multer";
 const storage = multer.diskStorage({
      filename: (req, file, callback) => {
          if(file) callback(null, new Date().toISOString() + '-' + file.originalname)
-         else callback(new Error('Invalid file'), '')
+         else callback(new Error('file is too large'), '')
      },
 })
 
 const upload = multer({
     storage,
     limits: {
-        fileSize: 2 * 1024 * 1024
+        fileSize: 10 * 1024 * 1024
     }
 })
 

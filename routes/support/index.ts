eb91be893ec4507afdebe865caf150c70c62d102
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-09-30 14:48:04 +0100

    support-fix-v2

--- routes/support/index.ts
+++ routes/support/index.ts
@@ -11,7 +11,7 @@ route.get('/get-supports', getSupportPosts)
 route.get('/get-support/:supportId', getSingleSupportPost)
 route.get('/user-supports', auth, getUserSupportPosts)
 route.patch('/update-support-status', auth, updateSupportStatusValidations, updateSupportStatus)
-route.patch('/extend-support-date/:supportId', auth, extendSupportDate)
+route.post('/extend-support-date/:supportId', auth, extendSupportDate)
 route.post('/create-support', auth, upload.array('files'), supportSchemaValidations,createSupportPost)
 route.delete('/user-support/:supportId', auth, upload.array('files'), deleteSupportPost)
 
